af20b3eab6c9f06d48e2a9f2dea773d8a2990293
bleafumb <ljh20000109@163.com>
2021-04-29 21:17:49 +0800

    bleafumb's test

---
+++ app/src/main/java/cn/edu/buct/se/cs1808/RoundImageView.java
@@ -0,0 +1,71 @@
+package com.example.myapplication;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.Xfermode;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+
+public class RoundImageView extends androidx.appcompat.widget.AppCompatImageView {
+
+    float width,height;
+    private int radius;
+    public RoundImageView(Context context) {
+        this(context, null);
+    }
+
+    public RoundImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public RoundImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        if (Build.VERSION.SDK_INT < 18) {
+            setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+        TypedArray typedArray=context.obtainStyledAttributes(attrs,R.styleable.RoundImageView);
+        if(typedArray!=null){
+            radius = typedArray.getInt(R.styleable.RoundImageView_image_radius,12);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        width = getWidth();
+        height = getHeight();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        if (width > radius && height > radius) {
+            Path path = new Path();
+            path.moveTo(radius, 0);
+            path.lineTo(width - radius, 0);
+            path.quadTo(width, 0, width, radius);
+            path.lineTo(width, height - radius);
+            path.quadTo(width, height, width - radius, height);
+            path.lineTo(radius, height);
+            path.quadTo(0, height, 0, height - radius);
+            path.lineTo(0, radius);
+            path.quadTo(0, 0, radius, 0);
+            canvas.clipPath(path);
+        }
+
+        super.onDraw(canvas);
+    }
+}
