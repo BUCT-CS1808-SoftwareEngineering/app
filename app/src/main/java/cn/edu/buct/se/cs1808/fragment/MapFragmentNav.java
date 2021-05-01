b316f3bd25bada17e5e68f74ffbd23cd076dfd28
essay <dazw2000@163.com>
2021-05-01 15:32:02 +0800

    删除多余Activity

--- app/src/main/java/cn/edu/buct/se/cs1808/fragment/MapFragmentNav.java
+++ app/src/main/java/cn/edu/buct/se/cs1808/fragment/MapFragmentNav.java
@@ -9,11 +9,40 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.baidu.mapapi.map.MapView;
+
 import cn.edu.buct.se.cs1808.R;
 
 public class MapFragmentNav extends NavBaseFragment {
+    private MapView mapView = null;
+
     public MapFragmentNav() {
         activityId = R.layout.activity_map;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mapView = (MapView) findViewById(R.id.bmapView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
     }
 
     @Override
