a5295c1fc3d19a0f09ce153fb55703f2d8dd30c4
bleafumb <ljh20000109@163.com>
2021-05-03 11:03:47 +0800

    博物馆详情界面

--- app/src/main/java/cn/edu/buct/se/cs1808/fragment/MuseumDetailFragment.java
+++ app/src/main/java/cn/edu/buct/se/cs1808/fragment/MuseumDetailFragment.java
@@ -4,19 +4,76 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 import cn.edu.buct.se.cs1808.R;
+import cn.edu.buct.se.cs1808.RoundImageView;
 
 public class MuseumDetailFragment extends Fragment{
     private int activityId;
+    private ArrayList<RoundImageView> objectImageArray= new ArrayList<>();
+    private ArrayList<TextView> objectNameArray= new ArrayList<>();
+    private RoundImageView exhibitionImage;
+    private TextView exhibitionName;
     public MuseumDetailFragment(){
         activityId = R.layout.activity_museum_detail;
     }
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(activityId, container, false);
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(activityId,container,false);
+    }
+
+    @Override
+    public void onViewCreated( @Nullable View view, Bundle savedInstanceState) {
+        objectImageArray.add((RoundImageView) view.findViewById(R.id.museum_detail_objectimage1));
+        objectImageArray.add((RoundImageView) view.findViewById(R.id.museum_detail_objectimage2));
+        objectImageArray.add((RoundImageView) view.findViewById(R.id.museum_detail_objectimage3));
+        objectImageArray.add((RoundImageView) view.findViewById(R.id.museum_detail_objectimage4));
+
+        objectNameArray.add((TextView) view.findViewById(R.id.museum_detail_objectname1));
+        objectNameArray.add((TextView) view.findViewById(R.id.museum_detail_objectname2));
+        objectNameArray.add((TextView) view.findViewById(R.id.museum_detail_objectname3));
+        objectNameArray.add((TextView) view.findViewById(R.id.museum_detail_objectname4));
+
+        exhibitionImage = (RoundImageView) view.findViewById(R.id.museum_detail_exhibitionimage);
+        exhibitionName = (TextView) view.findViewById(R.id.museum_detail_exhibitionname);
+        setObject();
+        setExhibition();
+
+        ArrayList<TextView> textArray = new ArrayList<>();
+        textArray.add((TextView) view.findViewById(R.id.museum_detail_graph));
+        textArray.add((TextView) view.findViewById(R.id.museum_detail_objectmore));
+        textArray.add((TextView) view.findViewById(R.id.museum_detail_exhibitionmore));
+        for(int i=0;i<textArray.size();i++){
+            TextView tv = textArray.get(i);
+            tv.setTextColor(android.graphics.Color.parseColor("#ee7712"));
+        }
+
+
+    }
+    private void setObject(){
+        String defaultName = "测试藏品测试藏品测试藏品测试藏品";
+        int defaultImage = R.drawable.bleafumb_object;
+        int num = objectImageArray.size();
+        for(int i=0;i<num;i++){
+            RoundImageView oImage = objectImageArray.get(i);
+            oImage.setImageResource(defaultImage);
+            TextView oName = objectNameArray.get(i);
+            oName.setText(defaultName);
+        }
+    }
+    private void setExhibition(){
+        String defaultName = "测试展览测试展览测试展览测试展览测试展览测试展览测试展览测试展览测试展览测试展览";
+        int defaultImage = R.drawable.bleafumb_exhibition;
+        exhibitionImage.setImageResource(defaultImage);
+        exhibitionName.setText(defaultName);
     }
 }
---
+++ app/src/main/java/cn/edu/buct/se/cs1808/utils/CompatViewPager.java
@@ -0,0 +1,35 @@
+package cn.edu.buct.se.cs1808.utils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.viewpager.widget.ViewPager;
+
+public class CompatViewPager extends ViewPager {
+    public CompatViewPager(@NonNull Context context) {
+        super(context);
+    }
+
+    public CompatViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int height = 0;
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            int h = child.getMeasuredHeight();
+            if (h > height)
+                height = h;
+        }
+
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
