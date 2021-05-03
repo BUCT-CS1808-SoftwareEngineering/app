652b67a006268d3e3b5095150e9cc949f97e21e7
essay <dazw2000@163.com>
2021-05-03 15:28:26 +0800

    地图界面添加点击事件处理

--- app/src/main/java/cn/edu/buct/se/cs1808/fragment/MapFragmentNav.java
+++ app/src/main/java/cn/edu/buct/se/cs1808/fragment/MapFragmentNav.java
@@ -4,10 +4,12 @@ import android.graphics.Bitmap;
 import android.graphics.Outline;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewOutlineProvider;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
@@ -17,6 +19,7 @@ import androidx.fragment.app.Fragment;
 import com.baidu.mapapi.map.MapView;
 
 import cn.edu.buct.se.cs1808.R;
+import cn.edu.buct.se.cs1808.RoundImageView;
 import cn.edu.buct.se.cs1808.components.MapRecentCard;
 import cn.edu.buct.se.cs1808.utils.LoadImage;
 import cn.edu.buct.se.cs1808.utils.RoundView;
@@ -24,6 +27,8 @@ import cn.edu.buct.se.cs1808.utils.RoundView;
 public class MapFragmentNav extends NavBaseFragment {
     private MapView mapView = null;
     private LinearLayout cardsView;
+    private RoundImageView searchButton;
+    private EditText searchInput;
 
     public MapFragmentNav() {
         activityId = R.layout.activity_map;
@@ -36,6 +41,17 @@ public class MapFragmentNav extends NavBaseFragment {
         mapView = (MapView) findViewById(R.id.bmapView);
         cardsView = (LinearLayout) findViewById(R.id.mapCardsView);
         RoundView.setRadius(24, mapView);
+
+        // 搜索按钮点击事件
+        searchButton = (RoundImageView) findViewById(R.id.mapSearchButton);
+        searchInput = (EditText) findViewById(R.id.mapSearchInput);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String q = searchInput.getText().toString();
+                search(q);
+            }
+        });
 
         addCards("北京故宫博物院", "北京市", "北京故宫博物馆", "http://7q5evw.com1.z0.glb.clouddn.com/images/article/FtPbcYX5VeTM6CfEBsCVi2aGRj0n.jpg");
         addCards("国家博物馆", "北京市", "国家博物馆", "https://pic.baike.soso.com/ugc/baikepic2/26022/cut-20190829122815-1940041223_jpg_751_600_36257.jpg/300");
@@ -50,8 +66,23 @@ public class MapFragmentNav extends NavBaseFragment {
         LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) mapRecentCard.getLayoutParams();
         lp.setMarginEnd(32);
         mapRecentCard.setLayoutParams(lp);
+        // 添加卡片点击事件
+        mapRecentCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cardOnClick(mapRecentCard);
+            }
+        });
     }
 
+    private void search(String q) {
+        if (q == null || q.length() == 0) return;
+        Log.i("Map Search", q);
+    }
+    private void cardOnClick(MapRecentCard mapRecentCard) {
+        if (mapRecentCard == null) return;
+        Log.i("Card Click", mapRecentCard.getMuseumName());
+    }
     @Override
     public void onResume() {
         super.onResume();
