df267d28696bd3e31b221a0ae4b5281b17b065e0
essay <dazw2000@163.com>
2021-05-05 16:08:08 +0800

    地图添加点标记功能

---
+++ app/src/main/java/cn/edu/buct/se/cs1808/utils/BitmapUtil.java
@@ -0,0 +1,20 @@
+package cn.edu.buct.se.cs1808.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+
+public class BitmapUtil {
+    public static Bitmap zoomImg(Bitmap bm, int newWidth, int newHeight) {
+        //获得图片的宽高
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        //计算缩放比例
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        //取得想要缩放的matrix参数
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth, scaleHeight);
+        //得到新的图片
+        return Bitmap.createBitmap(bm, 0, 0, width, height, matrix, true);
+    }
+}
