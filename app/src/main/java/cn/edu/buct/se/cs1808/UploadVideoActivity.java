17305161a8a545add62c41d93db12c68b9a3c0e7
essay <dazw2000@163.com>
2021-05-11 16:50:38 +0800

    完成上传视频页面的视频组件

--- app/src/main/java/cn/edu/buct/se/cs1808/UploadVideoActivity.java
+++ app/src/main/java/cn/edu/buct/se/cs1808/UploadVideoActivity.java
@@ -1,21 +1,36 @@
 package cn.edu.buct.se.cs1808;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.VideoView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+
+import cn.edu.buct.se.cs1808.components.VideoViewPlus;
+import cn.edu.buct.se.cs1808.utils.DensityUtil;
 import cn.edu.buct.se.cs1808.utils.RoundView;
+import cn.edu.buct.se.cs1808.utils.UriToFilePath;
+import cn.edu.buct.se.cs1808.utils.VideoUtil;
 
 public class UploadVideoActivity extends AppCompatActivity {
     private TextView publicButton;
     private ImageView backButton;
     private EditText introduceInput;
-    private VideoView selectedVideo;
+    private VideoViewPlus selectedVideo;
     private EditText videoTitleInput;
     private TextView selectedMuseumName;
     private TextView selectMuseumButt;
@@ -28,11 +43,19 @@ public class UploadVideoActivity extends AppCompatActivity {
         publicButton = (TextView) findViewById(R.id.publicButton);
         backButton = (ImageView) findViewById(R.id.backButton);
         introduceInput = (EditText) findViewById(R.id.introduceInput);
-        selectedVideo = (VideoView) findViewById(R.id.selectedVideo);
+        selectedVideo = (VideoViewPlus) findViewById(R.id.selectedVideo);
+        selectedVideo.setVerticalHeight(400);
         videoTitleInput = (EditText) findViewById(R.id.videoTitleInput);
         selectedMuseumName = (TextView) findViewById(R.id.selectedMuseumName);
         selectMuseumButt = (TextView) findViewById(R.id.selectMuseumButt);
         initRadius();
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            String path = intent.getStringExtra("path");
+            selectedVideo.play(path);
+            selectedVideo.setVideoImage(path);
+        }
     }
 
     private void initRadius() {
