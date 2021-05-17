fc8aa0a38f492825ed40a494d2320de388d7f093
bleafumb <ljh20000109@163.com>
2021-05-17 14:37:12 +0800

    首页数据库连接

--- app/src/main/java/cn/edu/buct/se/cs1808/fragment/IndexFragmentNav.java
+++ app/src/main/java/cn/edu/buct/se/cs1808/fragment/IndexFragmentNav.java
@@ -34,6 +34,9 @@ public class IndexFragmentNav extends NavBaseFragment {
     private LinearLayout museumContainer;
     private TextView textMore;
     private ImageView searchButton;
+    private JSONArray museumInfoArrayScore;
+    private JSONArray museumInfoArrayClick;
+    private int indexNum; 
 
     public IndexFragmentNav() {
         activityId = R.layout.activity_index;
@@ -46,6 +49,10 @@ public class IndexFragmentNav extends NavBaseFragment {
         museumContainer = (LinearLayout) view.findViewById(R.id.main_museum_container);
         textMore = (TextView) view.findViewById(R.id.main_text_more);
         searchButton = (ImageView) view.findViewById(R.id.main_search_button);
+        museumInfoArrayScore = new JSONArray();
+        museumInfoArrayClick = new JSONArray();
+        indexNum = 1;
+
         addSearchBox(10);
         addMuseumBox(10);
         TextView lookMore = view.findViewById(R.id.main_text_more);
@@ -73,44 +80,13 @@ public class IndexFragmentNav extends NavBaseFragment {
         });
     }
 
-    public static int dip2px(Context context, float dpValue) {
-        final float scale = context.getResources().getDisplayMetrics().density;
+    public static int dip2px(Context context, double dpValue) {
+        final double scale = context.getResources().getDisplayMetrics().density;
         return (int) (dpValue * scale + 0.5f);
     }
+
     private void addSearchBox(int num){
-        int defaultImage = R.drawable.bleafumb_main_2;
-        String defaultName = "测试博物馆";
-        String defaultScore = "9.9";
-        int defaultGrade=4;
-        int image,grade;
-        String name,score;
-        image = defaultImage;
-        grade = defaultGrade;
-        name = defaultName;
-        score = defaultScore;
-        for(int i=0;i<num;i++){
-            grade = (i+1);
-            BoxTest searchBox = new BoxTest(ctx);
-            searchBox.setAttr(image,name,grade,score);
-            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(dip2px(ctx,120), dip2px(ctx,150));
-            lp.setMargins(0, 0, dip2px(ctx,10), 0);
-            searchBox.setLayoutParams(lp);
-            searchContainer.addView(searchBox);
-        }
-    }
-    private void addMuseumBox(int num){
 
-        //加载失败时的默认值
-        int defaultImage = R.drawable.bleafumb_main_3;
-        String defaultName = "暂无数据";
-        String defaultScore = "0.0";
-        String defaultText = "暂无数据";
-
-//        //请求失败时显示默认值
-//        image = defaultImage;
-//        text = defaultText;
-//        name = defaultName;
-//        score = defaultScore;
 
         JSONObject params = new JSONObject();
         try {
@@ -125,35 +101,68 @@ public class IndexFragmentNav extends NavBaseFragment {
             try{
                 JSONObject info = rep.getJSONObject("info");
                 JSONArray items = info.getJSONArray("items");
-                JSONObject top = items.getJSONObject(0);
-                for(int i=0;i<items.length();i++){
-                    JSONObject it = items.getJSONObject(i);
-                    //用于加载的值
-                    int image;
-                    String name,text,score;
-                    image = defaultImage;
-                    name = it.getString("muse_Name");
-                    text = it.getString("muse_Intro");
-                    score = defaultScore;
-                    //新建对象
-                    MuseumCard museumCard = new MuseumCard(ctx);
-                    //设置属性
-                    museumCard.setAttr(image,name,text,score);
-                    RelativeLayout.LayoutParams lp=new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                    museumCard.setLayoutParams(lp);
-                    //获取自定义类内元素绑定事件
-                    RoundImageView rImage = museumCard.getMuseumImage();
-                    TextView mName = museumCard.getMuseumName();
-                    rImage.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            openMuseumActivity(ctx);
-                        }
-                    });
-                    museumContainer.addView(museumCard);
 
+                int pos[] = new int[1];
+                for(int i=0;i<items.length();i++){
+                    pos[0] = i;
+
+                    JSONObject it = items.getJSONObject(i);
+
+
+                    double score[] = new double[1];
+                    JSONObject scoreParams = new JSONObject();
+                    try{
+                        scoreParams.put("muse_ID",it.getInt("muse_ID"));
+                    }
+                    catch (JSONException e){
+
+                    }
+                    ApiTool.request(ctx, ApiPath.GET_MUSEUM_SCORE, scoreParams, (JSONObject repScore) -> {
+                        // 请求成功，rep为请求获得的数据对象
+                        try{
+                            JSONObject infoScore = repScore.getJSONObject("info");
+                            JSONObject itemsScore = infoScore.getJSONObject("items");
+                            double score1 = itemsScore.getDouble("env_Review");
+                            double score2 = itemsScore.getDouble("exhibt_Review");
+                            double score3 = itemsScore.getDouble("service_Review");
+                            score[0] = (score1+score2+score3)/3;
+                            try{
+                                it.put("muse_Score",score[0]);
+                                museumInfoArrayClick.put(museumInfoArrayClick.length(),it);
+                            }
+                            catch (JSONException e){
+
+                            }
+                        }
+                        catch(JSONException e){
+                            int max=1,min=0;
+                            double ran2 = (double) (Math.random()*(max-min)+min);
+                            score[0] = 4+ran2;
+                            try{
+                                it.put("muse_Score",score[0]);
+                                museumInfoArrayClick.put(museumInfoArrayClick.length(),it);
+                            }
+                            catch (JSONException ee){
+
+                            }
+                        }
+                        generateSearchBox(it,pos[0]);
+                    }, (JSONObject error) -> {
+                        // 请求失败
+                        int max=1,min=0;
+                        double ran2 = (double) (Math.random()*(max-min)+min);
+                        score[0] = 4+ran2;
+                        try{
+                            it.put("muse_Score",score[0]);
+                            museumInfoArrayClick.put(museumInfoArrayClick.length(),it);
+                            ;
+                        }
+                        catch (JSONException e){
+
+                        }
+                        generateSearchBox(it,pos[0]);
+                    });
                 }
-                //Log.e("66666666666", top.getString("muse_Name"));
             }
             catch(JSONException e){
 
@@ -163,23 +172,164 @@ public class IndexFragmentNav extends NavBaseFragment {
 
         });
 
-//        for(int i=0;i<num;i++){
-//            MuseumCard museumCard = new MuseumCard(ctx);
-//            //设置属性
-//            museumCard.setAttr(image,name,text,score);
-//            RelativeLayout.LayoutParams lp=new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-//            museumCard.setLayoutParams(lp);
-//            //获取自定义类内元素绑定事件
-//            RoundImageView rImage = museumCard.getMuseumImage();
-//            TextView mName = museumCard.getMuseumName();
-//            rImage.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    openMuseumActivity(ctx);
-//                }
-//            });
-//            museumContainer.addView(museumCard);
-//        }
+
+    }
+    private void addMuseumBox(int num){
+
+        JSONObject params = new JSONObject();
+        try {
+            params.put("pageSize", num);
+            params.put("pageIndex", indexNum);
+        }
+        catch (JSONException e) {
+
+        }
+        ApiTool.request(ctx, ApiPath.GET_MUSEUM_INFO, params, (JSONObject rep) -> {
+            // 请求成功，rep为请求获得的数据对象
+            try{
+                JSONObject info = rep.getJSONObject("info");
+                JSONArray items = info.getJSONArray("items");
+
+                for(int i=0;i<items.length();i++){
+                    JSONObject it = items.getJSONObject(i);
+
+                    double score[] = new double[1];
+                    JSONObject scoreParams = new JSONObject();
+                    try{
+                        scoreParams.put("muse_ID",it.getInt("muse_ID"));
+                    }
+                    catch (JSONException e){
+
+                    }
+                    ApiTool.request(ctx, ApiPath.GET_MUSEUM_SCORE, scoreParams, (JSONObject repScore) -> {
+                        // 请求成功，rep为请求获得的数据对象
+                        try{
+                            JSONObject infoScore = repScore.getJSONObject("info");
+                            JSONObject itemsScore = infoScore.getJSONObject("items");
+                            double score1 = itemsScore.getDouble("env_Review");
+                            double score2 = itemsScore.getDouble("exhibt_Review");
+                            double score3 = itemsScore.getDouble("service_Review");
+                            score[0] = (score1+score2+score3)/3;
+                            try{
+                                it.put("muse_Score",score[0]);
+                                museumInfoArrayScore.put(museumInfoArrayScore.length(),it);
+                            }
+                            catch (JSONException e){
+
+                            }
+                        }
+                        catch(JSONException e){
+                            int max=1,min=0;
+                            double ran2 = (double) (Math.random()*(max-min)+min);
+                            score[0] = 4+ran2;
+                            try{
+                                it.put("muse_Score",score[0]);
+                                museumInfoArrayScore.put(museumInfoArrayScore.length(),it);
+                            }
+                            catch (JSONException ee){
+
+                            }
+                        }
+                        generateMuseumBox(it);
+
+
+                    }, (JSONObject error) -> {
+                        // 请求失败
+                        int max=1,min=0;
+                        double ran2 = (double) (Math.random()*(max-min)+min);
+                        score[0] = 4+ran2;
+                        try{
+                            it.put("muse_Score",score[0]);
+                            museumInfoArrayScore.put(museumInfoArrayScore.length(),it);
+                        }
+                        catch (JSONException e){
+
+                        }
+                        generateMuseumBox(it);
+                    });
+
+                }
+                indexNum+=1;
+            }
+            catch(JSONException e){
+
+            }
+        }, (JSONObject error) -> {
+            // 请求失败
+
+        });
+
+    }
+
+
+    private void generateSearchBox(JSONObject it,int pos){
+        //加载失败时的默认值
+        int defaultImage = R.drawable.bleafumb_main_2;
+        String defaultName = "暂无数据";
+        String defaultScore = "0.0";
+        String defaultText = "暂无数据";
+        try {
+            int defaultGrade = 4;
+            int image, grade;
+            String name, score;
+
+            image = defaultImage;
+            name = it.getString("muse_Name");
+            score = String.format("%.1f",it.getDouble("muse_Score"));
+            grade = defaultGrade;
+
+            grade = (pos + 1);
+            BoxTest searchBox = new BoxTest(ctx);
+            searchBox.setAttr(image, name, grade, score);
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(dip2px(ctx, 120), dip2px(ctx, 150));
+            lp.setMargins(0, 0, dip2px(ctx, 10), 0);
+            searchBox.setLayoutParams(lp);
+
+            searchBox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openMuseumActivity(ctx);
+                }
+            });
+            searchContainer.addView(searchBox);
+        }
+        catch (JSONException e){
+
+        }
+    }
+    private void generateMuseumBox(JSONObject it){
+        //加载失败时的默认值
+        int defaultImage = R.drawable.bleafumb_main_3;
+        String defaultName = "暂无数据";
+        String defaultScore = "0.0";
+        try{
+            int image;
+            String name,text,grade;
+            image = defaultImage;
+            name = it.getString("muse_Name");
+            text = it.getString("muse_Intro");
+            grade = String.format("%.1f",it.getDouble("muse_Score"));
+
+            //新建对象
+            MuseumCard museumCard = new MuseumCard(ctx);
+            //设置属性
+            museumCard.setAttr(image,name,text,grade);
+            RelativeLayout.LayoutParams lp=new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            museumCard.setLayoutParams(lp);
+            //获取自定义类内元素绑定事件
+            RoundImageView rImage = museumCard.getMuseumImage();
+            TextView mName = museumCard.getMuseumName();
+            museumCard.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openMuseumActivity(ctx);
+                }
+            });
+            museumContainer.addView(museumCard);
+        }
+        catch(JSONException e){
+
+        }
     }
     public static void openMuseumActivity(Context context) {
         //页面跳转
